42331e3654f6228ad2da23fcfa1c56b74c880a42
AmirSolo456 <amirsol456@gmail.com>
2026-06-10 05:42:09 +0330

    script

---
+++ DamKhatoonDb.App/dbo/Tables/dbo.Products.sql
@@ -0,0 +1,50 @@
+﻿ 
+ 
+CREATE TABLE [dbo].[Products](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](200) NOT NULL,
+	[Code] [nvarchar](50) NULL,
+	[Description] [nvarchar](max) NULL,
+	[Unit] [nvarchar](20) NULL,
+	[DefaultPrice] [decimal](18, 2) NULL,
+	[Version] [int] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[Price] [decimal](18, 2) NULL,
+	[Product_Code] [int] NOT NULL,
+	[TypeId] [int] NOT NULL,
+	[TypeName] [nvarchar](max) NOT NULL,
+	[CreatedAt] [datetime2](7) NOT NULL,
+	[UpdatedAt] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [DF_Products_Version]  DEFAULT ((1)) FOR [Version]
+GO
+
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [DF_Products_IsDeleted]  DEFAULT ((0)) FOR [IsDeleted]
+GO
+
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [DF_Products_Product_Code]  DEFAULT ((1)) FOR [Product_Code]
+GO
+
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [DF_Products_TypeId]  DEFAULT ((0)) FOR [TypeId]
+GO
+
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [DF_Products_CreatedAt]  DEFAULT (getdate()) FOR [CreatedAt]
+GO
+
+ALTER TABLE [dbo].[Products] ADD  CONSTRAINT [DF_Products_UpdatedAt]  DEFAULT (getdate()) FOR [UpdatedAt]
+GO
+
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Products_Type] FOREIGN KEY([TypeId])
+REFERENCES [dbo].[Products_Type] ([Id])
+GO
+
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Products_Type]
+GO
+
+
---
+++ DamKhatoonDb.App/dbo/Tables/dbo.Products_Type.sql
@@ -0,0 +1,39 @@
+﻿ 
+GO
+
+CREATE TABLE [dbo].[Products_Type](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Unit] [nvarchar](max) NOT NULL,
+	[Code] [int] NOT NULL,
+	[ParentId] [int] NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[CreatedAt] [datetime2](7) NOT NULL,
+	[UpdatedAt] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Products_Type] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Products_Type] ADD  CONSTRAINT [DF_Products_Type_Code]  DEFAULT ((1)) FOR [Code]
+GO
+
+ALTER TABLE [dbo].[Products_Type] ADD  CONSTRAINT [DF_Products_Type_IsDeleted]  DEFAULT ((0)) FOR [IsDeleted]
+GO
+
+ALTER TABLE [dbo].[Products_Type] ADD  CONSTRAINT [DF_Products_Type_CreatedAt]  DEFAULT (getdate()) FOR [CreatedAt]
+GO
+
+ALTER TABLE [dbo].[Products_Type] ADD  CONSTRAINT [DF_Products_Type_UpdatedAt]  DEFAULT (getdate()) FOR [UpdatedAt]
+GO
+
+ALTER TABLE [dbo].[Products_Type]  WITH CHECK ADD  CONSTRAINT [FK_Products_Type_Parent] FOREIGN KEY([ParentId])
+REFERENCES [dbo].[Products_Type] ([Id])
+GO
+
+ALTER TABLE [dbo].[Products_Type] CHECK CONSTRAINT [FK_Products_Type_Parent]
+GO
+
+
